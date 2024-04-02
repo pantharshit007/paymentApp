3f415a2d26a3657eabc2fa8bf71f4ef5b2baeded
pantharshit007 <aaryanpandit3527@gmail.com>
2024-04-02 15:49:27 +0530

    update readme- added images section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,12 @@ function Home() {
         <div className="flex justify-center items-center flex-col h-screen gap-y-1 bg-slate-300">
 
             <p className="font-bold italic text-pay-default">Hi Mom!</p>
-            <Link to={'/signin'} className="rounded-sm py-1 px-2 bg-pay-light text-white">Click to Sign In</Link>
+            <div className='flex space-x-4'>
+                <Link to={'/signin'} className="rounded-sm py-1 px-3 bg-pay-light text-white">
+                    Sign In</Link>
+                <Link to={'/dashboard'} className="rounded-sm py-1 px-2 bg-pay-light text-white">
+                    Dashboard</Link>
+            </div>
 
         </div>
     )
