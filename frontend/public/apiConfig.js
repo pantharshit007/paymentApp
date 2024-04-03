8036d6cb577c06178119f7f5830e70324252541c
pantharshit007 <aaryanpandit3527@gmail.com>
2024-04-03 20:20:32 +0530

    be-url

--- frontend/public/apiConfig.js
+++ frontend/public/apiConfig.js
@@ -1,5 +1,6 @@
 
 const PORT = 4000
-const BACKEND_URL = `http://localhost:${PORT}/api/v1/`
+// const BACKEND_URL = `http://localhost:${PORT}/api/v1/`
+const BACKEND_URL = `https://paymentapp-be.onrender.com/api/v1/`
 
 export default BACKEND_URL
