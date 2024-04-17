b4433d53907156ebab7ca6c1aa741a173c25f9a9
pantharshit007 <aaryanpandit3527@gmail.com>
2024-04-18 01:19:01 +0530

    updated the be url

--- frontend/public/apiConfig.js
+++ frontend/public/apiConfig.js
@@ -1,5 +1,5 @@
-
 const PORT = 4000
-const BACKEND_URL = `http://localhost:${PORT}/api/v1/`
+// const BACKEND_URL = `http://localhost:${PORT}/api/v1/`
+const BACKEND_URL = process.env.REACT_APP_BE_URL
 
 export default BACKEND_URL
