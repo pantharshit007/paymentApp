18704bb6dbae694fee0f0fd4cbe35bec221e2da0
Harshit Pant <97608579+pantharshit007@users.noreply.github.com>
2024-05-04 01:30:42 +0530

    Update apiConfig.js
    updated environment variable syntax

--- frontend/public/apiConfig.js
+++ frontend/public/apiConfig.js
@@ -1,5 +1,7 @@
 const PORT = 4000
 // const BACKEND_URL = `http://localhost:${PORT}/api/v1/`
-const BACKEND_URL = process.env.REACT_APP_BE_URL
+const BACKEND_URL = import.meta.env.VITE_BE_URL
+//For production shift this env value to a .env file in the root directory outside of src and then call upon it
+// const BACKEND_URL = process.env.REACT_APP_BE_URL
 
 export default BACKEND_URL
